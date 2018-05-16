186252b3b179103e6fb7ec04b0579ce7d5b33457
Prodev2017 <chris.bergman320@gmail.com>
2018-05-15 22:42:30 -0400

    add behaviour

---
+++ lib/simple_pool/worker_lookup_behaviour/default.ex
@@ -0,0 +1,8 @@
+#-------------------------------------------------------------------------------
+# Author: Keith Brings
+# Copyright (C) 2018 Noizu Labs, Inc. All rights reserved.
+#-------------------------------------------------------------------------------
+
+defmodule Noizu.SimplePool.WorkerLookupBehaviour.Default do
+  use Noizu.SimplePool.WorkerLookupBehaviour.DefaultImplementation
+end
